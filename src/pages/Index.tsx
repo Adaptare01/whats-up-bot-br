9303425e169ba8f13ddf0b77e81533db3a0a431c
Adaptare01 <adaptaresoftware@gmail.com>
2025-05-22 09:42:36 -0300

    Mudar mensagem

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,7 +54,7 @@ const Index = () => {
             className="bg-whatsapp-green text-white px-6 py-2 rounded-full shadow hover:bg-whatsapp-dark-green transition"
             onClick={() => {
               setStarted(true);
-              sendMessage('Olá quero inscrever minha ideia empreendedora');
+              sendMessage('Vamos Inovar');
             }}
           >
             Iniciar conversa
